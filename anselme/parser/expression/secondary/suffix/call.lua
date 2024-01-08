8562e8f18b8e5c35f113c2e8e93fbb335cacc002
Étienne Reuh Fildadut <fildadut@reuh.eu>
2024-01-08 16:41:44 +0100

    Allow function call with tuple and struct without parentheses

--- anselme/parser/expression/secondary/suffix/call.lua
+++ anselme/parser/expression/secondary/suffix/call.lua
@@ -2,6 +2,8 @@
 
 local secondary = require("anselme.parser.expression.secondary.secondary")
 local parenthesis = require("anselme.parser.expression.primary.parenthesis")
+local tuple = require("anselme.parser.expression.primary.tuple")
+local struct = require("anselme.parser.expression.primary.struct")
 
 local operator_priority = require("anselme.common").operator_priority
 
@@ -12,22 +14,32 @@ return secondary {
 	priority = operator_priority["_()"],
 
 	match = function(self, str, current_priority, primary)
-		return self.priority > current_priority and parenthesis:match(str)
+		return self.priority > current_priority and (parenthesis:match(str) or tuple:match(str) or struct:match(str))
 	end,
 
 	parse = function(self, source, str, limit_pattern, current_priority, primary)
 		local start_source = source:clone()
 		local args = ArgumentTuple:new()
 
-		local exp, rem = parenthesis:parse(source, str, limit_pattern)
+		local exp, rem
 
-		if Nil:is(exp) then
-			if str:match("^%(%s*%(%s*%)%s*%)") then -- special case: single nil argument
-				exp = Tuple:new(Nil:new())
-			else -- no arguments
-				exp = Tuple:new()
+		if parenthesis:match(str) then
+			exp, rem = parenthesis:parse(source, str, limit_pattern)
+
+			if Nil:is(exp) then
+				if str:match("^%(%s*%(%s*%)%s*%)") then -- special case: single nil argument
+					exp = Tuple:new(Nil:new())
+				else -- no arguments
+					exp = Tuple:new()
+				end
+			elseif not Tuple:is(exp) or exp.explicit then -- single argument
+				exp = Tuple:new(exp)
 			end
-		elseif not Tuple:is(exp) or exp.explicit then -- single argument
+		elseif tuple:match(str) then
+			exp, rem = tuple:parse(source, str, limit_pattern)
+			exp = Tuple:new(exp)
+		else
+			exp, rem = struct:parse(source, str, limit_pattern)
 			exp = Tuple:new(exp)
 		end
 
